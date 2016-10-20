e709f30400a9aebaf9331b25311f2eaa090c08ff
Roger Stringer <freekrai@me.com>
2016-10-20 12:51:57 -0700

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,3 +1,26 @@
+/*
+// Load the Visualization API and the corechart package.
+	google.charts.load('current', {'packages':['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'Topping');
+		data.addColumn('number', 'Slices');
+		data.addRows([
+			['Mushrooms', 3],
+			['Onions', 1],
+			['Olives', 1],
+			['Zucchini', 1],
+			['Pepperoni', 2]
+		]);
+		
+		var options = {'title':'How Much Pizza I Ate Last Night','width':400,'height':300};
+		// Instantiate and draw our chart, passing in some options.
+		var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+		chart.draw(data, options);
+	}
+*/
+
 (function() {
 	'use strict';
 	var app = angular.module('dashboardApp', [ 'flybaseResourceHttp' ])
@@ -24,14 +47,34 @@ API_KEY:"token:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlrZXkiOiJkMmZkYzY3Ni1m
 			$scope.init();
 
 			$scope.init_epoch = function( ){
+				google.charts.load('current', {'packages':['corechart']});
+				google.charts.setOnLoadCallback( $scope.update_epoch ;
+
+/*
 				$scope.epoch =  jQuery('#chart').epoch({
 					type: 'bar',
 					data: []
 				});
 				$scope.update_epoch();
+*/
 			};
 
 			$scope.update_epoch = function(){
+
+				var new_data = jQuery.map($scope.questions, function(question, i) {
+		            return [question.question, question.points];
+		        });
+
+				var data = new google.visualization.DataTable();
+				data.addColumn('string', 'Question');
+				data.addColumn('number', 'Points');
+				data.addRows( new_data );
+				
+				var options = {'title':'Questions','width':400,'height':300};
+				var chart = new google.visualization.PieChart(document.getElementById('chart'));
+				chart.draw(data, options);
+
+/*
 				var new_data = jQuery.map($scope.questions, function(question, i) {
 		            return {
 		                x: question.question,
@@ -43,6 +86,7 @@ API_KEY:"token:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlrZXkiOiJkMmZkYzY3Ni1m
 		            label: 'series',
 		            values: new_data
 		        }]);
+*/
 			};
 			Question.flybase().on('added', function( data ){
 				$timeout(function() {
